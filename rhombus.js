f94e436972f42982ffb95e91261bafc095280506
Spencer Phippen <spencer.phippen@gmail.com>
2014-11-21 16:15:06 -0700

    Added rhombus first interface

---
+++ rhombus.js
@@ -0,0 +1,91 @@
+//! rhombus.js
+//! authors: Spencer Phippen, Tim Grant
+//! license: MIT
+
+(function(context) {
+  var r = {};
+  context.rhombus = r;
+
+  r.startPlayback = function() {
+
+  };
+
+  r.stopPlayback = function() {
+
+  };
+
+  r.getPosition = function() {
+
+  };
+
+  r.moveToPosition = function(tick) {
+
+  };
+
+  r.getLoopEnabled = function() {
+
+  };
+
+  r.setLoopEnabled = function(enabled) {
+
+  };
+
+  r.getLoopStart = function() {
+
+  };
+
+  r.setLoopStart = function(tick) {
+
+  };
+
+  r.getLoopEnd = function() {
+
+  };
+
+  r.setLoopEnd = function() {
+
+  };
+
+  r.getNoteCount = function() {
+
+  };
+
+  r.getNote = function(index) {
+
+  };
+
+  r.insertNote = function(pitch, start, length) {
+
+  };
+
+  function Note(pitch, start, length) {
+
+  };
+
+  Note.prototype = {
+    getPitch: function() {
+
+    },
+
+    setPitch: function(pitch) {
+
+    },
+
+    getStart: function() {
+
+    },
+
+    setStart: function (start) {
+
+    },
+
+    getLength: function() {
+
+    },
+
+    setLength: function(length) {
+
+    }
+  };
+
+})(this);
