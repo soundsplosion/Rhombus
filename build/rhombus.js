96e4603482f5862962056d026ede16bfc3ec5afa
Spencer Phippen <spencer.phippen@gmail.com>
2014-12-08 11:07:18 -0700

    Added master gain control

--- build/rhombus.js
+++ build/rhombus.js
@@ -80,11 +80,20 @@
     var feedbackGain = r._ctx.createGain();
     feedbackGain.gain.value = 0.4;
 
-    inGain.connect(r._ctx.destination);
+    var masterOutGain = r._ctx.createGain();
+    r.getMasterGain = function() {
+      return masterOutGain.gain.value;
+    };
+    r.setMasterGain = function(gain) {
+      masterOutGain.gain.value = gain;
+    };
+
+    inGain.connect(masterOutGain);
     delay.connect(feedbackGain);
     feedbackGain.connect(delay);
     delay.connect(outGain);
-    outGain.connect(r._ctx.destination);
+    outGain.connect(masterOutGain);
+    masterOutGain.connect(r._ctx.destination);
 
     graph.mainout = inGain;
     r._graph = graph;
