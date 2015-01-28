e37eca6a1b4926e7563eec072b8bf5e8e682510b
Spencer Phippen <spencer.phippen@gmail.com>
2015-01-27 22:48:45 -0700

    Added instrument add/remove interface

--- src/rhombus.instrument.js
+++ src/rhombus.instrument.js
@@ -5,11 +5,89 @@
 (function(Rhombus) {
   Rhombus._instrumentSetup = function(r) {
 
-    function Instrument() {
-      Tone.PolySynth.call(this, 6, Tone.MonoSynth);
+    var typeMap = {
+      "mono" : Tone.MonoSynth,
+      "am"   : Tone.AMSynth,
+      "fm"   : Tone.FMSynth,
+      "pluck": Tone.PluckSynth,
+      "noise": Tone.NoiseSynth,
+      "samp" : Tone.MultiSampler,
+      "duo"  : Tone.DuoSynth
+    };
+
+    function Instrument(type, options) {
+      var ctr = typeMap[type];
+      if (ctr === null || ctr === undefined) {
+        ctr = Tone.MonoSynth;
+      }
+
+      this._ctr = ctr;
+      Tone.PolySynth.call(this, null, ctr);
+
       this.toMaster();
       this._triggered = {};
     }
+    Tone.extend(Instrument, Tone.PolySynth);
+
+    var instruments = [];
+
+    r.getInstruments = function() {
+      return instruments;
+    };
+
+    r.addInstrument = function(type, options) {
+      instr = new Instrument(type, options);
+
+      if (instr === null || instr === undefined) {
+        return;
+      }
+
+      instruments.push(instr);
+    };
+
+    function inToIndex(instrOrIndex) {
+      var index;
+      if (typeof instrOrIndex === "object") {
+        index = instruments.indexOf(instrOrIndex);
+      } else {
+        index = +index;
+      }
+      if (index >= instruments.length) {
+        index = -1;
+      }
+      return index;
+    }
+
+    r.removeInstrument = function(instrOrIndex) {
+      var index = inToIndex(instrOrIndex);
+      if (index < 0) {
+        return;
+      }
+
+      instruments.splice(index, 1);
+    };
+
+    r.moveInstrument = function(instrOrIndex, newIndex) {
+      var index = inToIndex(instrOrIndex);
+      if (index < 0) {
+        return;
+      }
+
+      var ins = instruments.splice(index, 1);
+      instruments.splice(newIndex, 0, ins);
+    };
+
+    r.swapInstruments = function(instrOrIndex1, instrOrIndex2) {
+      var i1 = inToIndex(instrToIndex1);
+      var i2 = inToIndex(instrToIndex2);
+      if (i1 < 0 || i2 < 0) {
+        return;
+      }
+
+      var tmp = instruments[i1];
+      instruments[i1] = instruments[i2];
+      instruments[i2] = tmp;
+    };
 
     Tone.extend(Instrument, Tone.PolySynth);
 
@@ -49,22 +127,27 @@
       this._triggered = {};
     };
 
-    var inst1 = new Instrument();
-    r.Instrument = inst1;
-
     // only one preview note is allowed at a time
     var previewNote = undefined;
 
     r.startPreviewNote = function(pitch) {
+      if (instruments.length === 0) {
+        return;
+      }
+
       if (previewNote === undefined) {
         previewNote = new Note(pitch, 0);
-        r.Instrument.triggerAttack(previewNote._id, pitch, 0);
+        instruments[0].triggerAttack(previewNote._id, pitch, 0);
       }
     };
 
     r.stopPreviewNote = function() {
+      if (instruments.length === 0) {
+        return;
+      }
+
       if (previewNote !== undefined) {
-        r.Instrument.triggerRelease(previewNote._id, 0);
+        instruments[0].triggerRelease(previewNote._id, 0);
         previewNote = undefined;
       }
     };
