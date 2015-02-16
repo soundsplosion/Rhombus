f93eb158a0fe6cc5b013b2e22d3f568bf8ce556f
timgrant <tim@rootdown.net>
2015-02-16 13:17:53 -0700

    Added a NaN check for BPM values.

--- src/rhombus.time.js
+++ src/rhombus.time.js
@@ -146,7 +146,8 @@
     }
 
     r.setBpm = function(bpm) {
-      if (notDefined(bpm) || isNull(bpm) || +bpm < 1 || +bpm > 1000) {
+      if (notDefined(bpm) || isNull(bpm) || isNaN(+bpm) || 
+          +bpm < 1 || +bpm > 1000) {
         console.log("[Rhomb] - Invalid tempo");
         return undefined;
       }
