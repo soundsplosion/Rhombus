e4416952d7a52c6a3b94988e52145eb483c4a492
Spencer Phippen <spencer.phippen@gmail.com>
2015-04-10 21:32:36 -0600

    Fixed typos that led to buggy graph behavior

--- build/rhombus.js
+++ build/rhombus.js
@@ -1043,7 +1043,7 @@
       for (var outputIdx = 0; outputIdx < go.length; outputIdx++) {
         var output = go[outputIdx];
         for (var portIdx = 0; portIdx < output.to.length; portIdx++) {
-          var port = output.to[i];
+          var port = output.to[portIdx];
           this.graphDisconnect(outputIdx, port.node, port.slot, true);
         }
       }
@@ -1051,7 +1051,7 @@
       for (var inputIdx = 0; inputIdx < gi.length; inputIdx++) {
         var input = gi[inputIdx];
         for (var portIdx = 0; portIdx < input.from.length; portIdx++) {
-          var port = input.from[i];
+          var port = input.from[portIdx];
           port.node.graphDisconnect(port.slot, this, inputIdx, true);
         }
       }
--- src/rhombus.graph.js
+++ src/rhombus.graph.js
@@ -230,7 +230,7 @@
       for (var outputIdx = 0; outputIdx < go.length; outputIdx++) {
         var output = go[outputIdx];
         for (var portIdx = 0; portIdx < output.to.length; portIdx++) {
-          var port = output.to[i];
+          var port = output.to[portIdx];
           this.graphDisconnect(outputIdx, port.node, port.slot, true);
         }
       }
@@ -238,7 +238,7 @@
       for (var inputIdx = 0; inputIdx < gi.length; inputIdx++) {
         var input = gi[inputIdx];
         for (var portIdx = 0; portIdx < input.from.length; portIdx++) {
-          var port = input.from[i];
+          var port = input.from[portIdx];
           port.node.graphDisconnect(port.slot, this, inputIdx, true);
         }
       }
