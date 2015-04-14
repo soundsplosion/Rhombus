c0234c4436026a493ec5b615c0650edf8394ab8b
Spencer Phippen <spencer.phippen@gmail.com>
2015-04-14 15:36:21 -0600

    Started script effect

---
+++ src/rhombus.effect.script.js
@@ -0,0 +1,8 @@
+//! rhombus.effect.script.js
+//! authors: Spencer Phippen, Tim Grant
+//! license: MIT
+(function (Rhombus) {
+  Rhombus._scriptEffectSetup = function(r) {
+
+  };
+})(this.Rhombus);
